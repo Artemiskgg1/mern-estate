eb83f25f5773dfd45397f2dc17e7df71ca4109d9
iynes <abhaghildiyal@gmail.com>
2024-09-03 19:51:12 +0530

    feat: Add user authentication routes and controllers

--- api/index.js
+++ api/index.js
@@ -2,6 +2,9 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
 
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+
 mongoose
   .connect(process.env.MONGO_DB_URI)
   .then(() => {
@@ -16,3 +19,6 @@ const app = express();
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
