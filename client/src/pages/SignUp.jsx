1b0445f162c5180509edcf4af2d2145b821671da
iynes <abhaghildiyal@gmail.com>
2024-09-10 23:30:53 +0530

    feat: Add error handling in SignIn and SignUp pages

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 function SignUp() {
   const [formData, setFormData] = useState({});
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     setFormData({
@@ -31,6 +32,8 @@ function SignUp() {
         return;
       }
       setLoading(false);
+      setError(null);
+      Navigate("/sign-in");
     } catch (error) {
       setLoading(false);
       setError(error.message);
@@ -72,7 +75,7 @@ function SignUp() {
       <div className="flex gap-2 mt-5">
         <p>Have an account?</p>
         <Link to={"/sign-in"}>
-          <span className="text-blue-700"> Sign in</span>
+          <span className="text-blue-700"> Sign up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-3">{error}</p>}
