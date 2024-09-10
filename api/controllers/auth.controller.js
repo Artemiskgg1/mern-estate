45462cbac0d86ce14f12ca2bdcfb0ba333a3849b
iynes <abhaghildiyal@gmail.com>
2024-09-10 23:11:44 +0530

    feat: Add JWT token generation and authentication in signin route

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -21,6 +22,15 @@ export const signin = async (req, res, next) => {
     if (!validUser) return next(errorHandler(401, "User not found!"));
     const validPassword = bcryptjs.compare(password, validUser.password);
     if (!validPassword) return next(errorHandler(401, "Invalid password!"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 3600000),
+      })
+      .status(200)
+      .json({ rest });
   } catch (error) {
     next(error);
   }
