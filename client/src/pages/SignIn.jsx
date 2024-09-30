16ebd3ff81287a6eec3172da8d8f7fa52581ef1e
iynes <abhaghildiyal@gmail.com>
2024-09-30 12:00:00 +0000

    Refactor SignIn component to fix duplicate import and update Redux action

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -4,7 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import {
   signInStart,
   signInFailure,
-  signInFailure,
+  signInSuccess,
 } from "../redux/user/userSlice";
 
 function SignIn() {
--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit";
 import useReducer from "./user/userSlice";
 
-export default configureStore({
+export const store = configureStore({
   reducer: { user: useReducer },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
