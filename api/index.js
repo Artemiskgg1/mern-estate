7d13e05e9bfb43d197b0048a17e3f307027e664a
iynes <abhaghildiyal@gmail.com>
2024-11-03 00:16:01 +0530

    Implement user update functionality with password hashing and add token verification middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import "dotenv/config";
+import cookieParser from "cookie-parser";
 
 import userRouter from "./routes/user.routes.js";
 import authRouter from "./routes/auth.routes.js";
@@ -17,6 +18,8 @@ mongoose
 const app = express();
 
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
